22015cff17986491c2172f76e92e4f22ef13fe71
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2024-03-29 22:56:25 +0200

    teeDC

--- dist/index.js
+++ dist/index.js
@@ -9,12 +9,5 @@ Object.defineProperty(exports, "LoginPage", {
     return _LoginPage["default"];
   }
 });
-Object.defineProperty(exports, "Register", {
-  enumerable: true,
-  get: function get() {
-    return _Register["default"];
-  }
-});
 var _LoginPage = _interopRequireDefault(require("./Pages/Login/LoginPage"));
-var _Register = _interopRequireDefault(require("./Pages/Register/Register"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
