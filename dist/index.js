59ae5f4c66ef41485a17f2d3b476375e9124516b
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2024-03-29 22:42:14 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,5 @@ Object.defineProperty(exports, "__esModule", {
 exports.Capitalize = void 0;
 var Capitalize = exports.Capitalize = function Capitalize(_ref) {
   var str = _ref.str;
-  return;
-  /*#__PURE__*/React.createElement("h1", null, str.charAt(0).toUpperCase() + str.slice(1));
+  return /*#__PURE__*/React.createElement("h1", null, str.charAt(0).toUpperCase() + str.slice(1));
 };
