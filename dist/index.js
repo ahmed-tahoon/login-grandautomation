788ff9fd903d4cec83fc91d844a2f783e4b81221
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2024-03-29 22:50:55 +0200

    v4t

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Capitalize = void 0;
-var Capitalize = exports.Capitalize = function Capitalize(_ref) {
-  var str = _ref.str;
-  return /*#__PURE__*/React.createElement("h1", null, str.charAt(0).toUpperCase() + str.slice(1));
-};
+Object.defineProperty(exports, "LoginPage", {
+  enumerable: true,
+  get: function get() {
+    return _LoginPage["default"];
+  }
+});
+Object.defineProperty(exports, "Register", {
+  enumerable: true,
+  get: function get() {
+    return _Register["default"];
+  }
+});
+var _LoginPage = _interopRequireDefault(require("./Pages/Login/LoginPage"));
+var _Register = _interopRequireDefault(require("./Pages/Register/Register"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 // login and register
 export { default as LoginPage } from './Pages/Login/LoginPage';
-export { default as Register } from './Pages/Register/Register';
