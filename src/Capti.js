eb6baf01dee8b2b384339291310f3fe42d7095f2
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2024-03-29 22:02:14 +0200

    v6

---
+++ src/Capti.js
@@ -0,0 +1,11 @@
+import React, { useContext, useEffect, useState } from "react";
+
+function Capti() {
+  return (
+    <div className="animate__fadeIn animate__animated animate__faster text-white h-full flex flex-col justify-center items-center md:px-0 px-5 bg-[#121212]">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default Capti;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export const Capitalize = ({ str }) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+import Capti from "./Capti";
+
+export { Capti };
